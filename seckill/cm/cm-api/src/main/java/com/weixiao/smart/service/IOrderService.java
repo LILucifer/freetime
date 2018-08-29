e734550d4c96ae76ac2a31e2f55d140837c1076f
LILucifer <lishixiang0925@126.com>
2018-08-29 08:17:03 +0800

    init

---
+++ seckill/cm/cm-api/src/main/java/com/weixiao/smart/service/IOrderService.java
@@ -0,0 +1,9 @@
+package com.weixiao.smart.service;
+
+/**
+ * @author lishixiang0925@126.com.
+ * @description (订单接口)
+ * @Created 2018-08-28 21:59.
+ */
+public interface IOrderService {
+}
---
+++ seckill/cm/cm-svc/src/main/java/com/weixiao/smart/main/Main.java
@@ -0,0 +1,19 @@
+package com.weixiao.smart.main;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author lishixiang0925@126.com.
+ * @description (这里用一句话描述这个类的作用)
+ * @Created 2018-08-28 21:17.
+ */
+public class Main {
+    private static Logger logger = LoggerFactory.getLogger(Main.class);
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:spring-context.xml");
+        context.start();
+        logger.info("cm-svc was started");
+    }
+}
