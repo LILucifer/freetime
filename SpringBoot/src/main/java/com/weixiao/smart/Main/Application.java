57a462349d85f35dff9d97d236b0cd31282d0da3
li <lishixiang0925@126.com>
2018-10-11 17:49:24 +0800

    测试全自动话发布功能

---
+++ SpringBoot/src/main/java/com/weixiao/smart/Main/Application.java
@@ -0,0 +1,20 @@
+package com.weixiao.smart.Main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author lishixiang
+ * @Title:
+ * @Description: TODO(这里用一句话描述这个类的作用)
+ * @date 2018/10/11 17:23
+ */
+@SpringBootApplication
+@ComponentScan
+public class Application {
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ SpringBoot/src/main/java/com/weixiao/smart/Main/AutoStart.java
@@ -0,0 +1,44 @@
+package com.weixiao.smart.Main;
+
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author lishixiang
+ * @Title:
+ * @Description: TODO(自动发布服务)
+ * @date 2018/10/11 14:59
+ */
+@Component
+@EnableScheduling
+public class AutoStart {
+
+    /**
+     * 定时任务检查服务重启
+     */
+    @Scheduled(cron = "0 0/2 * * * ? ")
+    public void checkRestart(){
+        System.out.println("checkRestart at " +  new SimpleDateFormat("yyyy-MM-dd HH:mm:ss ").format(new Date()));
+        System.exit(0);
+
+    }
+
+    /**
+     * 服务重启
+     */
+    @PreDestroy
+    public void reStart(){
+        System.out.println("gdsp was stoped");
+    }
+
+    @PostConstruct
+    public void construct(){
+        System.out.println("gdsp was constructed");
+    }
+}
