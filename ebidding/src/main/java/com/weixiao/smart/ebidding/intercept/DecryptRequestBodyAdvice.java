2292872da101e05a3ff5ff2bdc23e3c01478ea32
li <lishixiang0925@126.com>
2019-02-26 17:53:24 +0800

    ebidding update

---
+++ ebidding/src/main/java/com/weixiao/smart/ebidding/intercept/DecryptRequestBodyAdvice.java
@@ -0,0 +1,10 @@
+package com.weixiao.smart.ebidding.intercept;
+
+/**
+ * @author lishixiang
+ * @Title:
+ * @Description: TODO(这里用一句话描述这个类的作用)
+ * @date 2019/2/26 11:11
+ */
+public class DecryptRequestBodyAdvice {
+}
---
+++ ebidding/src/main/java/com/weixiao/smart/ebidding/intercept/EncryptResponseBodyAdvice.java
@@ -0,0 +1,10 @@
+package com.weixiao.smart.ebidding.intercept;
+
+/**
+ * @author lishixiang
+ * @Title:
+ * @Description: TODO(这里用一句话描述这个类的作用)
+ * @date 2019/2/26 16:40
+ */
+public class EncryptResponseBodyAdvice {
+}
