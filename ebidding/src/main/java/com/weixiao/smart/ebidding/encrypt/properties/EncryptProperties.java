feb71bdb3ff82e439d1385fdd39ade525509f389
LILucifer <lishixiang0925@126.com>
2019-02-28 23:25:35 +0800

    test encrypt and decrypt

---
+++ ebidding/src/main/java/com/weixiao/smart/ebidding/encrypt/properties/EncryptProperties.java
@@ -0,0 +1,9 @@
+package com.weixiao.smart.ebidding.encrypt.properties;
+
+/**
+ * @author lishixiang0925@126.com.
+ * @description (这里用一句话描述这个类的作用)
+ * @Created 2019-02-28 23:00.
+ */
+public class EncryptProperties {
+}
