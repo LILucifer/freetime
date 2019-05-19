0cffcb598404c9e69547f94030d49175e68d1862
LILucifer <lishixiang0925@126.com>
2019-05-19 12:50:05 +0800

    add mini-code project

---
+++ mini-code/src/main/java/com/weixiao/smart/SpringBootApplication.java
@@ -0,0 +1,18 @@
+package com.weixiao.smart;
+
+import com.weixiao.smart.minicode.MiniCodeTest;
+import org.springframework.boot.SpringApplication;
+
+/**
+ * @author lishixiang0925@126.com.
+ * @description (这里用一句话描述这个类的作用)
+ * @Created 2018-09-24 21:59.
+ */
+@org.springframework.boot.autoconfigure.SpringBootApplication
+public class SpringBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApplication.class, args);
+        MiniCodeTest test = new MiniCodeTest();
+        test.mainExecute();
+    }
+}
