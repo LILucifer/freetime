7b5ba70d28a092831dc2c6c661f7e421c7c96f13
li <lishixiang0925@126.com>
2020-03-30 18:00:10 +0800

    update

--- SpringBoot/src/test/java/TestString.java
+++ SpringBoot/src/test/java/TestString.java
@@ -1,8 +1,13 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 /**
  * @author lishixiang0925@126.com.
  * @description (这里用一句话描述这个类的作用)
  * @Created 2018-07-18 08:00.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
 public class TestString {
     public void buiderString(){
         String a = "ss";
@@ -11,5 +16,11 @@ public class TestString {
         a += "sjdk";
         StringBuilder b = new StringBuilder("jdhfjjdsf").append("sdasd");
         String c = b.toString();
+    }
+    @Test
+    public void testSubstring(){
+        String path = "/manage/NoticeInfo/list";
+        path = path.substring(0,path.lastIndexOf("/"));
+        System.out.println(path);
     }
 }
