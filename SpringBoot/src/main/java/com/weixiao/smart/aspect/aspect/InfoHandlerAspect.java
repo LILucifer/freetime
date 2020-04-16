995822af5dca90acdd090cb879e06b9244cbbf70
li <lishixiang0925@126.com>
2020-04-16 18:21:59 +0800

    aspect demo

--- SpringBoot/src/main/java/com/weixiao/smart/aspect/aspect/InfoHandlerAspect.java
+++ SpringBoot/src/main/java/com/weixiao/smart/aspect/aspect/InfoHandlerAspect.java
@@ -2,15 +2,13 @@ package com.weixiao.smart.aspect.aspect;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 /**
  * @author lishixiang0925@126.com.
- * @description (这里用一句话描述这个类的作用)
+ * @description AOP测试，获取pointCut方法参数，方法名，类名
  * @Created 2019-04-20 10:24.
  */
 @Aspect
@@ -25,11 +23,27 @@ public class InfoHandlerAspect {
 
     @Before("logAspect()")
     public  void doBefore(JoinPoint joinPoint){
+        log.info("doBefore");
+        log.info("doBefore this method target = {}", joinPoint.getTarget());
         log.info("doBefore this method args = {}", joinPoint.getArgs());
+
+    }
+    @Around("execution(* com.weixiao.smart.aspect..*Service.*(..))")
+    public void AroundRun(ProceedingJoinPoint joinPoint){
+        log.info("AroundRun");
+        try {
+            log.info("AroundRun this method target = {}", joinPoint.getTarget().getClass().toString());
+            log.info("AroundRun this method args = {}", joinPoint.getArgs());
+            log.info("AroundRun this method result = {}", joinPoint.proceed().toString());
+            log.info("AroundRun this method toShortString = {}", joinPoint.toShortString());
+            log.info("AroundRun this method toShortString = {}", joinPoint.toLongString());
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
     }
 
     @AfterReturning("execution(* com.weixiao.smart.aspect..*Service.*(..))")
-    public void afterRun(){
+    public void afterRun(JoinPoint joinPoint){
         log.info("afterRun");
     }
 }
---
+++ spring-boot-redis/src/main/java/com/weixiao/smart/controller/FileUploadController.java
@@ -0,0 +1,16 @@
+package com.weixiao.smart.controller;
+
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * @author lishixiang
+ * @Title:
+ * @Description: TODO(这里用一句话描述这个类的作用)
+ * @date 2020/4/11 21:53
+ */
+public class FileUploadController {
+
+    public void upload(MultipartFile file) {
+
+    }
+}
