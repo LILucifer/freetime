195257a3432be6394654d245fdff056360cb0193
LILucifer <lishixiang0925@126.com>
2020-09-23 08:45:53 +0800

    resposebility chain

---
+++ SpringBoot/src/main/java/com/weixiao/smart/basedatatype/responsibilitychain/Mail.java
@@ -0,0 +1,39 @@
+package com.weixiao.smart.basedatatype.responsibilitychain;
+
+/**
+ * @author lishixiang0925@126.com.
+ * @description (这里用一句话描述这个类的作用)
+ * @Created 2020-09-22 22:50.
+ */
+public class Mail {
+    enum Readability {}
+
+    enum Address {
+
+    }
+
+    enum Scanability {}
+
+    enum GetAddress {
+
+    }
+
+    Readability readability;
+    Address address;
+    Scanability scanability;
+    GetAddress getAddress;
+
+
+    public static Mail randomMails(){
+
+    }
+
+    public static Iterable<Mail> generator(int count) {
+
+    }
+
+
+
+
+
+}
---
+++ SpringBoot/src/main/java/com/weixiao/smart/basedatatype/responsibilitychain/ResponsibilityChainByEnum.java
@@ -0,0 +1,9 @@
+package com.weixiao.smart.basedatatype.responsibilitychain;
+
+/**
+ * @author lishixiang0925@126.com.
+ * @description (这里用一句话描述这个类的作用)
+ * @Created 2020-09-22 22:15.
+ */
+public class ResponsibilityChainByEnum {
+}
