402f305efaa481b7b44655a7f12841cd4f1fffae
LILucifer <lishixiang0925@126.com>
2020-09-24 08:19:41 +0800

    enum test

--- SpringBoot/src/main/java/com/weixiao/smart/basedatatype/responsibilitychain/Mail.java
+++ SpringBoot/src/main/java/com/weixiao/smart/basedatatype/responsibilitychain/Mail.java
@@ -1,21 +1,27 @@
 package com.weixiao.smart.basedatatype.responsibilitychain;
 
+
+
 /**
  * @author lishixiang0925@126.com.
  * @description (这里用一句话描述这个类的作用)
  * @Created 2020-09-22 22:50.
  */
 public class Mail {
-    enum Readability {}
-
-    enum Address {
-
+    enum Readability {
+        CAN_GET_READ,YES,NO1,NO2,NO3
     }
 
-    enum Scanability {}
+    enum Address {
+        GUANGZHOU,G1,G2,G3,NO
+    }
+
+    enum Scanability {
+        GOODIDEA,NO1,NO2,NO3
+    }
 
     enum GetAddress {
-
+        ADDRESS_RETURN,NO1,NO2
     }
 
     Readability readability;
@@ -25,11 +31,16 @@ public class Mail {
 
 
     public static Mail randomMails(){
-
+        Mail mail = new Mail();
+        return Enums.random(Readability.class);
     }
 
     public static Iterable<Mail> generator(int count) {
 
+    }
+    private Letter randomLetter() {
+        int pick = new Random().nextInt(Letter.values().length);
+        return Letter.values()[pick];
     }
 
 
