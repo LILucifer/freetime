cfc272ee0b1746397d839ec680ff648b00c23956
LILucifer <lishixiang0925@126.com>
2021-01-27 08:22:38 +0800

    update[C

---
+++ springcloud/spring-cloud-config-server/src/main/java/com/weixiao/samrt/springcloudconfigserver/controller/TestController.java
@@ -0,0 +1,9 @@
+package com.weixiao.samrt.springcloudconfigserver.controller;
+
+/**
+ * @author lishixiang0925@126.com.
+ * @description (这里用一句话描述这个类的作用)
+ * @Created 2021-01-27 00:05.
+ */
+public class TestController {
+}
